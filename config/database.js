588cb26a434422b8eb4a6b65e2b603e7ee058835
solsv7 <solsv7@gmail.com>
2025-07-10 12:43:33 -0300

    agrego config para docker

--- config/database.js
+++ config/database.js
@@ -4,6 +4,9 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'mariadb',
+    dialectOptions: {
+        allowPublicKeyRetrieval: true,
+    },
 });
 console.log('JWT_SECRET:', process.env.JWT_SECRET); 
 module.exports = sequelize;
