26640c9df561577fd318b41cc60326ab2ad2a98d
Lauu <163576941+Lauuti19@users.noreply.github.com>
2025-11-11 18:33:37 -0300

    debug videos

--- controllers/obtVidsController.js
+++ controllers/obtVidsController.js
@@ -5,10 +5,23 @@ const obtenerVideos = async (req, res) => {
     try {
         const response = await sequelize.query('CALL obtenerVideos()');
         res.status(200).json(response); 
-    } catch (error) {
-        console.error('Error al obtener los videos:', error);
-        res.status(500).json({ error: 'Error al obtener los videos'});
+    }} catch (error) {
+    console.error('Error al obtener los videos:');
+
+    if (error.original) {
+        console.error('Código del error SQL:', error.original.code);
+        console.error('Mensaje SQL:', error.original.sqlMessage);
+        console.error('SQL ejecutado:', error.original.sql);
+    } else {
+        console.error(error);
     }
+
+    res.status(500).json({
+        error: 'Error al obtener los videos',
+        detalle: error.original?.sqlMessage || error.message
+    });
+}
+
 };
 
 module.exports = obtenerVideos;
